82a149f0b9869e318a1c22828b7863d27601441a
SalahEddineGhamri <salaheddine.ghamri@yahoo.com>
2018-10-26 01:47:47 +0200

    Go!

--- Step_1/read_webcam.cpp
+++ Step_1/read_webcam.cpp
@@ -7,22 +7,25 @@ using namespace std;
 
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
+    VideoCapture cap(0);
+    if(!cap.isOpened())
         return -1;
 
     Mat edges;
+    Mat normal;
     namedWindow("edges",1);
+    namedWindow("normal", 1);
     for(;;)
     {
         Mat frame;
-        cap >> frame; // get a new frame from camera
+        cap >> frame;
         cvtColor(frame, edges, COLOR_BGR2GRAY);
         GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         Canny(edges, edges, 0, 30, 3);
         imshow("edges", edges);
+        imshow("normal", frame);
         if(waitKey(30) >= 0) break;
     }
-    // the camera will be deinitialized automatically in VideoCapture destructor
+
     return 0;
 }

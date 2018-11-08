bf9c3849c75b2f62169f6cff9599b9ed696a0587
salaheddineghamri <salaheddine.ghamri@yahoo.com>
2018-11-08 14:20:28 +0100

    feature detection

---
+++ opencvPython/Feature_detection/Sift_feature_detection.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread( "//home//salaheddineghamri//salaheddineghamri.jpg")
+gray_img = cv2. cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp = sift.detect(gray_img, None)
+
+outputimg = cv2.drawKeypoints(gray_img, kp, None)
+
+cv2.imshow("myimage", outputimg)
+cv2.waitKey(0)
